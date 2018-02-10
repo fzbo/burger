8c2bfd4d09a20c4626f957d4744bebb45014fe96
fzbo <elizabeth.bazan@gmail.com>
2018-02-09 23:32:29 -0500

    Update 1 server file, pack json

---
+++ server.js
@@ -0,0 +1,10 @@
+
+//DEPENDENCIES
+
+var express = require("express");
+var method-override = require("method-override");
+var body-parser = require("body-parser");
+//===========================================================//
+
+var port = process.env.port || 3000;
+var app = express();
