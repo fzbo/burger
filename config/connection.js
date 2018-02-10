9fbfe5a490d9a11539452df6b55dda8a6ecb718a
fzbo <elizabeth.bazan@gmail.com>
2018-02-10 08:08:12 -0500

    Update added MVC folders

---
+++ config/connection.js
@@ -0,0 +1,31 @@
+//PSUEDO FOR CONFIG FOLDER//
+//------------------------//
+	
+/*Config Setup
+
+
+Inside your burger directory, create a folder named config.
+Create a connection.js file inside config directory.
+
+
+
+Inside the connection.js file, setup the code to connect Node to MySQL.
+Export the connection.
+
+
+
+Create an orm.js file inside config directory.
+
+
+
+Import (require) connection.js into orm.js
+
+In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+
+
+
+selectAll() 
+
+insertOne() 
+
+updateOne()
